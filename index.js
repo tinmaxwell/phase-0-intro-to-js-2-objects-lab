0b5cf3a8791c922a29cb2c1e4721fab1ce39c73d
Max <tinmaxwell@gmail.com>
2023-12-21 16:40:59 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+let employee = {
+    name: "Max",
+    streetAddress: "1234 Guts",
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = Object.assign({}, employee);
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = Object.assign({}, employee);
+    delete updatedEmployee[key];
+    return updatedEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key];
+   return employee;
+  }
